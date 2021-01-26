5117713e1d0ff0bbdf29ff60088b0cf5effd79d0
20042345 <20042345@saultcollege.ca>
2021-01-26 07:38:23 +0000

    Final change Lab 3

--- form.php
+++ form.php
@@ -7,21 +7,22 @@
 	$fruits=	$fruit->get_all_fruits();
 	//print_r($fruits);
 	
-	if($_SERVER('REQUEST_METHOD')=='POST')
-	{
-		echo "Form submitted"
-	}
+	 if($_SERVER['REQUEST_METHOD']=='POST'){
+	 echo 'Form submitted';
+	 }
 	
 ?>
 <form method="post" action="form.php">  
+	<br>
+	<br>
+	
   <label for="Name">First Name</label> <br>
   <input type="text" name="firstname" id="firstname"><br>
-
+<br>
  <label for="Name">Last Name</label> <br>
  <input type="text" name="lasttname" id="lastname"><br>
-
+<br>
 	<label for="cars">Favorite fruit:</label>
-
  <select name="fruit" id="fruit_id">
   
   <?php
@@ -29,7 +30,10 @@
       <option value="<?= $name['id'] ?>"><?= $name['fruit'] ?></option>
   <?php
     } ?>
+	 
 </select> 
+	<br>
+	<br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 </body>
