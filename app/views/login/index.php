f766c6b392d9de440e9f64da338ecd94d890a9b9
20042345 <20042345@saultcollege.ca>
2021-02-23 20:06:58 +0000

    lab 5

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -3,11 +3,14 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>You are not logged in</h1>
-				<?php if ($_SESSION['failedAuth'])  ?>
-				<P>
-					Login failed. Use right username and password
-				</P>
+              <h1>You are not logged in</h1>
+				<?php if($_SESSION['failedAuth']){ ?>
+
+				<p>
+					Login failed.Please try again
+				</p>
+				<?php }  ?>
+
 				
             </div>
         </div>
