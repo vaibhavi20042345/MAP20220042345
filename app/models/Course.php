eca0406f02e1d42834a0a503b3b467aa0a2a52ab
20042345 <20042345@saultcollege.ca>
2021-03-05 03:50:57 +0000

    Course Links

--- app/models/Course.php
+++ app/models/Course.php
@@ -4,11 +4,9 @@
 		    public function __construct($param=false){}
 		
  		public function get_all_courses($department) {
-			//$department = $_REQUEST['department'];
  			$db = db_connect();
  			$statement = $db->prepare("select * from courses where department= :department;");
 			 $statement->execute(array(':department' => $department));
-			//$statement->bindParam(':department',$department);
  			$statement->execute();
  			$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
  			return $rows;
