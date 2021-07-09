ab327df2580c72ea80cf6708b06ff45135bbfa1d
20042345 <20042345@saultcollege.ca>
2021-07-09 04:22:55 +0000

    coupon change

--- models/Coupon.php
+++ models/Coupon.php
@@ -21,7 +21,7 @@
     // Create Post
     public function read_single() {
           // Create query
-          $query = "select * from Coupon where UserId='$this->UserId' and IsScratched=0";
+          $query = "select * from Coupon where UserId='$this->UserId'";
 
           // Prepare statement
           $stmt = $this->conn->prepare($query);
